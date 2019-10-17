dd3774c60e3c22f9700523c803330c1a6eafafeb
kmarks2013 <kmarks2013@gmail.com>
2019-10-17 16:48:30 -0400

    Static render from components

---
+++ src/components/Doctor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../logo.svg';
+
+export default function Doctor() {
+    return (
+        <div>
+            <h1>The Doctor</h1>
+            <img src={logo} alt="" />
+        </div>
+    )
+}
---
+++ src/components/DoctorBio.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DoctorBio() {
+    return (
+        <div>
+            This will be the Doctors Bio or greatest accomplishments or something
+        </div>
+    )
+}
