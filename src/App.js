d74a9751ea6923b191079a1d96c7f010b7f3b2d2
kmarks2013 <kmarks2013@gmail.com>
2019-10-17 20:04:41 -0400

    fetched doctors

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import DoctorContainer from './containers/DoctorContainer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <DoctorContainer />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API = "http://localhost:3000/doctors"
+
+class App extends React.Component {
+  state = {
+    doctors: []
+  }
+
+
+  componentDidMount() {
+    console.log("I mounted!")
+    fetch(API)
+    .then(res => res.json())
+    .then(doctorArr => {
+     console.log(doctorArr)
+    })
+  }
+
+
+
+  render() {
+    console.log(this.state)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <DoctorContainer doctors={this.state.doctors} />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/containers/DoctorContainer.js
+++ src/containers/DoctorContainer.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import DoctorList from '../components/DoctorList'
 
-export default class DoctorContainer extends Component {
+export default class DoctorContainer extends Component {    
+
     render() {
         return (
             <div className='doctor-container'>
