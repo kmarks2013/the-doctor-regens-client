6137955544480a80763f84f1510af83d17c30157
kmarks2013 <kmarks2013@gmail.com>
2019-12-20 16:23:22 -0500

    added bio title to doctor bio removed excess white space in navbar

--- src/components/DoctorBio.js
+++ src/components/DoctorBio.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function DoctorBio({doctor}) {
     return (
         <div className='doctor-bio'>
-            <h5></h5>
+            <h5>Bio</h5>
             <p>{doctor ? doctor.bio: null}</p>
             <a className='more-info' href={doctor ?  doctor.wikilink: null} target='_blank'>More Info!</a>
         </div>
