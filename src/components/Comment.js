722cfe1e49959dac767339c6bbffd46f8b7d7b4f
kmarks2013 <kmarks2013@gmail.com>
2019-12-20 09:40:35 -0500

    refactored comment tree

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,25 +1,22 @@
 import React, {Component} from 'react'
 
 export default class Comment extends Component{
-    // console.log(comment)
-        editDeleteButtons = () =>  {
-            if (this.props.loggedInUserId === this.props.comment.user.id){
-                return (  
-                    <div>
-                    <button onClick={(event) => this.props.editClick(event, this.props.comment)}> Edit Comment </button>
-                    <button onClick={(event) => this.props.deleteClick(event, this.props.comment)}>Delete Comment</button>
-                    </div>
-                )
-                } else{
-                   return null
-                }
-        }
+    editDeleteButtons = () =>  {
+        if (this.props.loggedInUserId === this.props.comment.user.id){
+            return (  
+                <div>
+                <button onClick={(event) => this.props.editClick(event, this.props.comment)}> Edit Comment </button>
+                <button onClick={(event) => this.props.deleteClick(event, this.props.comment)}>Delete Comment</button>
+                </div>
+            )
+            } else{
+                return null
+            }
+    }
     render() { 
     return (
         <div>
-            {/* <p> this should render the author name and date</p> */}
             <p className='comment-user'>{this.props.comment.user.username}</p>
-            {/* <p>this should render the content</p> */}
             <p className='comment-content'>{this.props.comment.content}</p>
             {this.editDeleteButtons()}
         </div>
--- src/containers/CommentContainer.js
+++ src/containers/CommentContainer.js
@@ -3,18 +3,14 @@ import CommentList from '../components/CommentList'
 import CommentForm from '../components/CommentForm'
 
 export default class CommentContainer extends Component {
-
     showForm = () => {
         if (this.props.loggedInUserId){
             return <CommentForm author={this.props.author} editComment={this.props.editComment} content={this.props.content} handleFormChange={this.props.handleFormChange} handleSubmit={this.props.handleSubmit} handleEditSubmit={this.props.handleEditSubmit}/>
         } else {
             return <h4>Create a new account to make a comment!</h4>
         } 
-
     } 
-
     render() {
-        // console.log(this.props.comments)
         return (
             <div className='comment-container'>
                 <div>
