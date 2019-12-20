39a268d7a81a7d9763c90fa86ff16a2d543ea212
kmarks2013 <kmarks2013@gmail.com>
2019-12-20 15:03:12 -0500

    submit is logging properly with correct form data. need to test post

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -4,10 +4,11 @@ import Comment from './Comment'
 export default class CommentList extends Component {
     renderComments = () => {
        return this.props.comments.map( (comment) => {
-            return <Comment comment={comment} loggedInUserId={this.props.loggedInUserId}editClick={this.props.editClick} deleteClick={this.props.deleteClick} key={`comment-${comment.id}`}/>
+            return <Comment comment={comment} loggedInUserId={this.props.loggedInUserId} editClick={this.props.editClick} deleteClick={this.props.deleteClick} key={`comment-${comment.id}`}/>
         })
     }
     render() {
+        console.log(this.props.comments)
         return (
             <div className='comments'>
             {this.renderComments()}
