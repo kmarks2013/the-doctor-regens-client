474689d3dcb77037b6da165014e0abc900a476d7
kmarks2013 <kmarks2013@gmail.com>
2019-12-20 14:52:24 -0500

    refactored choose doctor method

--- src/components/DoctorNames.js
+++ src/components/DoctorNames.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function DoctorNames({doctor, chooseDoctor}) {
     return (
         <div className='doctor-name'>
-            <button onClick={(event) => chooseDoctor(event, doctor)} >{doctor.name}</button>
+            <button onClick={() => chooseDoctor(doctor)} >{doctor.name}</button>
         </div>
     )
 }
