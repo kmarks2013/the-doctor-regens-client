3d583e781e75d3f8ff10e20b22220963c4ce6150
kmarks2013 <kmarks2013@gmail.com>
2019-12-21 11:03:40 -0500

    destructrued login component

--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-export default function LogInForm(props) {
+export default function LogInForm({logInSubmitted, onChange, username, password, logInOnOff }) {
     return (
         <div className='login'>
         <p>Log In </p>
-        <form className='login-form' onSubmit={props.logInSubmitted } >
+        <form className='login-form' onSubmit={logInSubmitted } >
             <label htmlFor='log_in_username'>Username</label>
-            <input type='text' onChange={props.onChange} name="username" value={props.username} /> 
+            <input type='text' onChange={onChange} name="username" value={username} /> 
             <label htmlFor='log_in_password'>Password</label>
-            <input type='password' onChange={props.onChange} name="password" value={props.password} />
+            <input type='password' onChange={onChange} name="password" value={password} />
             <input type="submit" />
         </form>
-        <button className='sign-up-button' onClick={ () => props.logInOnOff()} > Click to Create an Account</button>
+        <button className='sign-up-button' onClick={ () => logInOnOff()} > Click to Create an Account</button>
     </div>
     )
 }
