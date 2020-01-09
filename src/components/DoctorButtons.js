bdf8301269288b05cea4536e86362527810ee900
kmarks2013 <kmarks2013@gmail.com>
2020-01-09 10:44:48 -0500

    Added 'soon' and removed extra spaces to alert

--- src/components/DoctorButtons.js
+++ src/components/DoctorButtons.js
@@ -4,11 +4,11 @@ export default function DoctorButtons() {
 
     return (
     <div className='pull-to-open'>
-        <img onClick={() => alert("We are curretnly updating! Please check back soon for more  content!")} src='./images/pullToOpen.jpg' alt='' />
+        <img onClick={() => alert("We are curretnly updating! Please check back soon for more content!")} src='./images/pullToOpen.jpg' alt='' />
         <br/>
-        <button onClick={() => alert("We are currrently updating Please check back for more  content!")} > Sonic Devices!</button>
+        <button onClick={() => alert("We are currrently updating Please check back soon for more content!")} > Sonic Devices!</button>
         <br/>
-        <button onClick={() => alert("We are currrently updating Please check back for more  content!")}> Companions!</button>
+        <button onClick={() => alert("We are currrently updating Please check back soon for more content!")}> Companions!</button>
     </div>
     )
 }
