3855fa4d408c305bac2779084e52c869e4c693e1
kmarks2013 <kmarks2013@gmail.com>
2020-01-28 10:58:33 -0500

    calls login after logout

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -6,7 +6,7 @@ import User from '../components/User'
 export default class NavBar extends Component {
         logout = () => {
             localStorage.clear()
-            this.forceUpdate()
+            this.login
         }
 
         login = () => {
